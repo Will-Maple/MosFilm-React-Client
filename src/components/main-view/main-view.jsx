112030cedc0f409a0f371ce11ae9240b725fb5a7
Will-Maple17 <will.maple17@gmail.com>
2024-11-20 13:24:28 -0600

    Update main-view.jsx, movie-card.jsx, movie-view.jsx added open and close for movie-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card"
+import { MovieView } from "../movie-view/movie-view"
 
 export const MainView = () => {
   const [movies, setMovies] = useState([
-    { id: 1, title: "movie 1", author: "director1", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
-    { id: 2, title: "movie 2", author: "director2", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
-    { id: 3, title: "movie 3", author: "director3", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
-    { id: 4, title: "movie 4", author: "director4", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
-    { id: 5, title: "movie 5", author: "director5", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
-    { id: 6, title: "movie 6", author: "director6", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" }
+    { id: 1, title: "movie 1", director: "director1", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
+    { id: 2, title: "movie 2", director: "director2", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
+    { id: 3, title: "movie 3", director: "director3", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
+    { id: 4, title: "movie 4", director: "director4", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
+    { id: 5, title: "movie 5", director: "director5", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" },
+    { id: 6, title: "movie 6", director: "director6", image: "https://images.smiletemplates.com/uploads/screenshots/1/0000001466/poster-templates-s.jpg" }
   ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView movie={selectedBook} onBackClick={() => setSelectedMovie(null)} />
+    );
+  }
 
   if (movies.length === 0) {
     return <div>The list is empty! Oh no!</div>
@@ -18,13 +27,16 @@ export const MainView = () => {
 
   return (
     <>
-      <div>
-        <h1>Доброе день</h1>
-        {movies.map((movie) => {
-          return <MovieCard movie={movie} />;
-        })}
-      </div>
-      <button>Test</button>
+      <h1>Доброе день</h1>
+      {movies.map((movie) => {
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />;
+      })}
     </>
   )
 }
