036c245b5cf31c6c98693b5e5ece6e4200c7e6d7
Will-Maple17 <will.maple17@gmail.com>
2024-11-20 10:23:23 -0600

    update main-view.jsx added books usestate and maps

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 export const MainView = () => {
+  const [books, setBooks] = useState([
+    { id: 1, title: "movie 1" },
+    { id: 2, title: "movie 2" },
+    { id: 3, title: "movie 3" },
+    { id: 4, title: "movie 4" },
+    { id: 5, title: "movie 5" },
+    { id: 6, title: "movie 6" }
+  ]);
+
+  if (books.length === 0) {
+    return <div>The list is empty! Oh no!</div>
+  }
+
   return (
     <>
       <div>
-        <div>Доброе день</div>
-        <div>Movie1</div>
-        <div>Movie2</div>
-        <div>Movie3</div>
-        <div>Movie4</div>
-        <div>Movie5</div>
+        <h1>Доброе день</h1>
+        {books.map((book) => {
+          return <div key={book.id}>{book.title}</div>;
+        })}
       </div>
       <button>Test</button>
     </>
