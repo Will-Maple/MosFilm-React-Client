85eb584b60557c2604caa7f780ec4769c8118981
Will-Maple17 <will.maple17@gmail.com>
2024-11-24 15:28:57 -0600

    Update main-view.jsx added routing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,7 +5,7 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { Row, Col, Image } from "react-bootstrap";
-import { BrowersRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowersRouter, Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import Github from "./github.svg";
 import Portfolio from "./portfolio.svg";
 import Youtube from "./youtube.svg";
@@ -28,7 +28,6 @@ export const MainView = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        /*console.log(data)*/
         const moviesFromApi = data.map((doc) => {
           return {
             id: doc._id,
@@ -43,65 +42,109 @@ export const MainView = () => {
   }, [token]);
 
   return (
-    <Row className="justify-content-md-center">
-      {!user ? (
-        <>
-          <Col md={5}>
-            <LoginView onLoggedIn={(user, token) => {
-              setUser(user);
-              setToken(token);
-            }}
-            />
-          </Col>
-          <Col md={5}>
-            <SignupView />
-          </Col>
-        </>
-      ) : selectedMovie ? (
-        <Col md={8}>
-          <MovieView
-            movie={selectedMovie} onBackClick={() => setSelectedMovie(null)}
+    <BrowserRouter>
+      <Row className="justify-content-md-center">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <>
+                    <Col md={5}>
+                      <SignupView />
+                    </Col>
+                  </>
+                )}
+              </>
+            }
           />
-        </Col>
-      ) : movies.length === 0 ? (
-        <div>The list is empty! Oh no!</div>
-      ) : (
-        <>
-          <Col className="text-center mb-3" md={2}>
-            <div>
-              <a href="https://will-maple.github.io/Portfolio-Website/portfolio.html">
-                <Image src={Portfolio} alt="Portfolio Link" style={{ height: "50px", width: "100px" }} />
-              </a>
-            </div>
-            <div>
-              <a href="https://github.com/Will-Maple">
-                <Image src={Github} alt="Github Link" style={{ height: "50px", width: "100px" }} />
-              </a>
-            </div>
-          </Col>
-          <Col className="text-center mb-3" md={6}>
-            <h1 className="display-1">Доброе день</h1>
-          </Col>
-          <Col className="text-center mb-3" md={2}>
-            <div>
-              <a href="https://www.youtube.com/channel/UCm5U4zqpahzyNXBv5ZT51Jw">
-                <Image src={Youtube} alt="Youtube Link" style={{ height: "65px", width: "150px" }} />
-              </a>
-            </div>
-            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
-          </Col>
-          {movies.map((movie) => (
-            <Col className="mb-4" key={movie.id} md={3}>
-              <MovieCard
-                movie={movie}
-                onMovieClick={(newSelectedMovie) => {
-                  setSelectedMovie(newSelectedMovie);
-                }}
-              />
-            </Col>
-          ))}
-        </>
-      )}
-    </Row>
+          <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView onLoggedIn={(user, token) => {
+                      setUser(user);
+                      setToken(token);
+                    }}
+                    />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/movie/:movieID"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty! Oh no!</Col>
+                ) : (
+                  <Col md={8}>
+                    <MovieView
+                      movie={movies}
+                    />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty! Oh no!</Col>
+                ) : (
+                  <>
+                    <Col className="text-center mb-3" md={2}>
+                      <div>
+                        <a href="https://will-maple.github.io/Portfolio-Website/portfolio.html">
+                          <Image src={Portfolio} alt="Portfolio Link" style={{ height: "50px", width: "100px" }} />
+                        </a>
+                      </div>
+                      <div>
+                        <a href="https://github.com/Will-Maple">
+                          <Image src={Github} alt="Github Link" style={{ height: "50px", width: "100px" }} />
+                        </a>
+                      </div>
+                    </Col>
+                    <Col className="text-center mb-3" md={6}>
+                      <h1 className="display-1">Доброе день</h1>
+                    </Col>
+                    <Col className="text-center mb-3" md={2}>
+                      <div>
+                        <a href="https://www.youtube.com/channel/UCm5U4zqpahzyNXBv5ZT51Jw">
+                          <Image src={Youtube} alt="Youtube Link" style={{ height: "65px", width: "150px" }} />
+                        </a>
+                      </div>
+                      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+                    </Col>
+                    {movies.map((movie) => (
+                      <Col className="mb-4" key={movie.id} md={3}>
+                        <MovieCard
+                          movie={movie} />
+                      </Col>
+                    ))}
+                  </>
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </Row>
+    </BrowserRouter>
   );
 };
+
+/*selectedMovie*/
