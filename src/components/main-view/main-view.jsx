55f5ac92c51241def4b616f0528e6479da66cae4
Will-Maple17 <will.maple17@gmail.com>
2024-11-26 22:14:25 -0600

    Update main-view.jsx incorporated navbar

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,8 +4,9 @@ import { MovieCard } from "../movie-card/movie-card"
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import { Row, Col, Image } from "react-bootstrap";
-import { BrowersRouter, Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Github from "./github.svg";
 import Portfolio from "./portfolio.svg";
 import Youtube from "./youtube.svg";
@@ -43,6 +44,14 @@ export const MainView = () => {
 
   return (
     <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear()
+        }}
+      />
       <Row className="justify-content-md-center">
         <Routes>
           <Route
