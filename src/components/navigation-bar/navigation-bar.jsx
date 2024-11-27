4740374d2b6c87b60d4c302e1b719e3601817d38
Will-Maple17 <will.maple17@gmail.com>
2024-11-26 22:01:59 -0600

    Created and initialized navigation-bar.jsx

---
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,34 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router';
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <Navbar expand="md" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Доброе день</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {!user && (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login User
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signup">
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+            {user && (
+              <>
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/"></Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
