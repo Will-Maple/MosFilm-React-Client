8cccdd4ed82f6d84c5f04d13b4f48dc3dd3e2940
Will-Maple17 <will.maple17@gmail.com>
2024-11-26 22:46:20 -0600

    Update navbar.jsx to import from react-router-dom

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,5 +1,5 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
